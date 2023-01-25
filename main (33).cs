7d3eb7daf3159bd865546b60b857d8b1cda01a0b
Robert Dico <114947225+eljefe1111@users.noreply.github.com>
2023-01-25 13:53:13 -0500

    Add files via upload

---
+++ main (33).cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    //-------------q6--------------
+    for(int i = 0; i <= 100; i++)
+    {
+      Console.WriteLine(i);
+    }
+    Console.WriteLine("There are 50 odd numbers");
+    Console.WriteLine("There are 50 even numbers");
+    }
+}
